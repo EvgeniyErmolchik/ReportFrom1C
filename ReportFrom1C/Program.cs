7eacc51ded487edaa727243cf2c9cc306cfa39c7
e.ermolchik <e.ermolchik@hotmail.com>
2018-10-26 12:19:14 +0300

    World

--- ReportFrom1C/Program.cs
+++ ReportFrom1C/Program.cs
@@ -10,7 +10,7 @@ namespace ReportFrom1C
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
